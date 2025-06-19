9780626d29016f23e113cce982c8cd8764e97aa5
carlosm <carlosalcides190304@gmail.com>
2025-06-19 18:53:13 -0400

    add: the file to SQLserver Connection
    fix: the route to import the function in the app.js

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,6 @@
 import express, { json } from "express";
 import { corsMidleware } from "./middelware/cors";
-import { connectToDatabase } from "./modules/SQLServer/modules";
+import { connectToDatabase } from "./config/SQLserverConection";
 
 connectToDatabase()
 
