902ef4cd98e171e7d3350a4ddb35eac2ab58df6e
Alfonso José Palma Alvarado <sjalvarado08@hotmail.com>
2025-06-19 00:13:58 -0400

    Base frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" > 
+      <nav>Prueba numero 2</nav>
+      <p>Tarjetas</p>
+      <p>campos</p>
+      <p>boton</p>
+    </div>
+  );
+}
+
+export default App;
