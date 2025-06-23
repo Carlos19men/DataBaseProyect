140b352cdcc9ec3883fe009349b3676afaafad75
ElJako11 <joaquimejuhah@gmail.com>
2025-06-22 22:43:09 -0400

    EmployeeController created and some fixes in Empleados

---
+++ backend/controllers/EmployeeController.ts
@@ -0,0 +1,63 @@
+import {Request, Response} from 'express';
+import {employee} from '../models/Empleados';
+
+interface Employee {
+    CI: string;
+    name: string | null;
+    lastName: string | null;
+    RIF_establishment: string | null;
+}
+
+export class EmployeeController {
+    model: employee;
+
+    constructor(model: employee) {
+        this.model = model;
+    }
+
+    getAll = async(req: Request, res: Response<Employee[] | {message: string}>): Promise<void> => {
+       try {
+        const employees: Employee[] = await employee.getAll();
+
+        if(!employees || employees.length === 0){
+            res.status(404).json({"message": "No se encontraron empleados"});
+            return;
+        }
+
+        res.status(200).json(employees);
+        return;
+       } catch (error){
+         console.error("Error al obtener clientes", error)
+         res.status(500).json({message: "Error interno del servidor al encontrar empleados"});
+         return;
+       }
+
+    }
+
+    getbyCI = async(req: Request, res: Response<Employee | {message: string}>): Promise<void> => {
+        const {CI} = req.params;
+
+        if(CI != null) {
+            if(!CI || CI.length === 0) {
+                res.status(400).json({"message": "Se requiere la cédula del empleado"});
+                return;
+            }
+        }
+
+        try {
+            const employee_data: Employee = await employee.getByCI(CI);
+
+            if(!employee_data){
+                res.status(404).json({message: "No se consiguió un empleado con esa cédula"})
+                return;
+            }
+
+            res.status(200).json(employee_data);
+            return;
+        } catch(error) {
+            console.error("Error al obtener el empleado con esa CI", error);
+            res.status(500).json({message: "Error interno del servidor al conseguir un empleado"});
+            return;
+        }
+    }
+}
--- backend/models/Empleados.ts
+++ backend/models/Empleados.ts
@@ -1,7 +1,6 @@
-import { copyFileSync } from "fs";
 import { getDbPool } from "../config/SQLserverConection";
 
-export class Empleado {
+export class employee {
 
     static async getAll() {
         const pool = getDbPool();
@@ -10,7 +9,7 @@ export class Empleado {
         return result['recordset'];
     }
 
-    static async getByCI({ CI }: { CI: string; }) {
+    static async getByCI(CI: string) {
         if (CI === undefined || CI === null || CI.length === 0) {
             return { error: "Se necesita la cédula" };
         }
@@ -18,9 +17,9 @@ export class Empleado {
         const request = getDbPool().request();
         request.input('CI', CI);
 
-        const result = await request.query('SELECT * from Empleados where CI_emp = @CI;');
+        const result = await request.query('SELECT CI_emp, nombre, apellido, RIF_establecimiento from Empleados where CI_emp = @CI;');
         console.log(result['recordset']);
-        return result['recordset'];
+        return result['recordset'][0];
     }
 
     static async getbyRIF({RIF}: {RIF: string | null}){
