cfd17d2161aa55672f56aa6a9eebd8a537a55dea
jimenaazocar05 <jimenaazocar05@gmail.com>
2025-06-23 22:56:42 -0400

    Terminado metodos de pago

---
+++ backend/models/MetodoPagos.ts
@@ -0,0 +1,103 @@
+import {  getDbPool } from "../config/SQLserverConection";
+
+export class paymentMethods{
+    static async getAll(){
+
+        const result = await getDbPool().query('SELECT * FROM obtenerMetodosPago;')
+
+        console.log(result['recordset'])
+
+        return result['recordset']
+    }
+
+    static async getAllOrderByFactura(){
+
+        const result = await getDbPool().query('SELECT * FROM obtenerMetodosPago ORDER BY FacturaCorrespondiente;')
+
+        console.log(result['recordset'])
+
+        return result['recordset']
+    }
+
+    static async getByClient(id_cliente:number){
+        if ((id_cliente === null) || (id_cliente === undefined)) {
+            return('error: Client is required');
+        } else {
+            const request = getDbPool().request();
+
+            request.input('@id_cliente',id_cliente);
+
+            const result = await request.query('SELECT * FROM ObtenerMetodoPagoPorCliente(@id_cliente);')
+   
+            console.log(result['recordset']);
+            return result['recordset'][0] || { error: "Buy orden not found" };
+        }
+    }
+
+    static async create({tipo_moneda,monto_ef,fechaPago_Tar,tipo_tarjeta,banco,nro_tarjeta,monto_tar,referenciaPM,fecha_PM,monto_PM,telefono}:{tipo_moneda:string,monto_ef:number,fechaPago_Tar:string,tipo_tarjeta:string,banco:string,nro_tarjeta:string,monto_tar:number,referenciaPM:string,fecha_PM:string,monto_PM:number,telefono:string}){
+        //request
+        const request = getDbPool().request();
+
+        //inputs
+        request.input('tipo_moneda',tipo_moneda);
+        request.input('monto_ef',monto_ef);
+        request.input('fechaPago_Tar',fechaPago_Tar);
+        request.input('tipo_tarjeta',tipo_tarjeta);
+        request.input('banco',banco);
+        request.input('nro_tarjeta',nro_tarjeta);
+        request.input('monto_tar',monto_tar);
+        request.input('referenciaPM',referenciaPM);
+        request.input('fecha_PM',fecha_PM);
+        request.input('monto_PM',monto_PM);
+        request.input('telefono',telefono);
+
+        const result = await request.query('EXEC nuevaMetodoPago @tipo_moneda,@monto_ef,@fechaPago_Tar,@tipo_tarjeta,@banco,@nro_tarjeta,@monto_tar,@referenciaPM,@fecha_PM,@monto_PM,@telefono;')
+
+        console.log(result['recordset'])
+
+        return result['recordset']
+    }
+
+    static async edit({id_pago,tipo_moneda,monto_ef,fechaPago_Tar,tipo_tarjeta,banco,nro_tarjeta,monto_tar,referenciaPM,fecha_PM,monto_PM,telefono}:{id_pago: number,tipo_moneda:string,monto_ef:number,fechaPago_Tar:string,tipo_tarjeta:string,banco:string,nro_tarjeta:string,monto_tar:number,referenciaPM:string,fecha_PM:string,monto_PM:number,telefono:string}){
+        //request
+        const request = getDbPool().request();
+
+        //inputs
+        request.input('id_pago',id_pago);
+        request.input('tipo_moneda',tipo_moneda);
+        request.input('monto_ef',monto_ef);
+        request.input('fechaPago_Tar',fechaPago_Tar);
+        request.input('tipo_tarjeta',tipo_tarjeta);
+        request.input('banco',banco);
+        request.input('nro_tarjeta',nro_tarjeta);
+        request.input('monto_tar',monto_tar);
+        request.input('referenciaPM',referenciaPM);
+        request.input('fecha_PM',fecha_PM);
+        request.input('monto_PM',monto_PM);
+        request.input('telefono',telefono);
+
+        const result = await request.query('EXEC editarMetodoPago @id_pago,@tipo_moneda,@monto_ef,@fechaPago_Tar,@tipo_tarjeta,@banco,@nro_tarjeta,@monto_tar,@referenciaPM,@fecha_PM,@monto_PM,@telefono;')
+
+        console.log(result['recordset'])
+
+        return result['recordset']
+    }
+
+    static async delete(id_pago:number){
+        if ((id_pago === null) || (id_pago === undefined)) {
+            return {error: 'Numero de pago is required'}
+        }
+
+        //request
+        const request = getDbPool().request();
+
+        //inputs
+        request.input('id_pago',id_pago);
+
+        const result = await request.query('EXEC eliminarMetodoPago @id_pago;')
+
+        console.log(result['recordset'])
+
+        return result['recordset']
+    }
+}
