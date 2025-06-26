fa336fa832776a3ef7b78aa5461b11269b298f6c
carlosm <carlosalcides190304@gmail.com>
2025-06-25 21:39:06 -0400

    add: hice pull a lo de joquim para los modelos de vehiculos

--- backend/controllers/modelosController.ts
+++ backend/controllers/modelosController.ts
@@ -1,84 +1,100 @@
-import {Request,Response} from 'express'
-import { ModelsModel } from '../models/Modelos'
+import {Request, Response} from 'express';
+import { ModelsModel } from '../models/Modelos';
 
-
-interface Model{
-    id_marca:number,
-    marca: string,
-    id_modelo:number,
-    modelo:string,
-    oil_box: string,
-    oil_motor:string,
-    octanaje:string,
-    type_refrigerant:string,
-    description:string,
-    weight: number
-    nro_seat:number
+interface Model {
+    id_marca: number;
+    nro_modelo : number;
+    nombre: string;
+    aceite_caja: string;
+    aceite_motor: string;
+    octanaje: string;
+    tipo_refrigerante: string;
+    peso: number;
+    descripcion: string;
+    nro_puesto: number;
 }
 
-export class ModelController {
+export class ModelsController {
+    model: ModelsModel;
 
-    getAll = async (_req: Request, res: Response<Model[] | { message: string } | {error:string}>): Promise<void> => {
-        try{
-            const models:Model[] = await ModelsModel.getAll()
+    constructor(model: ModelsModel){
+        this.model = model;
+    }
 
-            if(!models){
-                res.status(404).json({error:'Modelos no encontrados'})
-                return
+    getAll = async (_req: Request, res: Response<Model[] | {message: string}>): Promise<void> => {
+        try {
+            const result: Model[] = await ModelsModel.getAll();
+
+            if(!result){
+                res.status(404).json({ message: 'No se encontraron modelos' });
+                return;
             }
-            res.status(200).send(models)
-            return
-        }catch (err){
-            console.log(err)
-            res.status(500).json({error:'internal error server'+err})
-            return
+            res.status(200).json(result);
+        } catch (error) {
+            console.error('Error al obtener los modelos:', error);
+            res.status(500).json({ message: 'Error al obtener los modelos'});
         }
     }
 
-    getById = async (req: Request, res: Response<Model | { message: string } | {error:string}>): Promise<void> => {
-        const {id_marca, id_modelo} = req.params
+    getbyID = async(req: Request, res: Response<Model | {message: string}>): Promise<void> => {
+        const id_marca = parseInt(req.params.id_marca, 10);
+        const id_modelo = parseInt(req.params.id_modelo, 10);
 
         try{
-            const model:Model = await ModelsModel.getById(parseInt(id_marca),parseInt(id_modelo))
+            const result: Model = await ModelsModel.getById(id_marca,id_modelo);
 
-            if(!model){
-                res.status(404).json({error:'Modelo no econetrado'})
-                return 
+            if (!result) {
+                res.status(404).json({ message: 'Modelo no encontrado' });
+                return;
+            }
+            res.status(200).json(result);
+        } catch(error) {
+            console.error("Ha ocurrido un error", error);
+            res.status(500).json({message: ""});
+        }
+    }
+
+    getbyMarca = async(req: Request, res: Response<Model[] | {message: string}>): Promise<void> => {
+        //const id_marca = parseInt(req.params.id_marca, 10);
+        const id_marca = req.params.id_marca;
+
+        try {
+            const result = await ModelsModel.getByMarca(id_marca);
+
+            if('message' in result){
+                res.status(400).json({message: result.message});
+                return;
             }
 
-            res.status(200).send(model)
-            return
-        }catch (err){
-            console.log(err)
-            res.status(500).json({error:'Internal server error' + err})
-            return 
+            const brand_data: Model[] = result;
+            res.status(200).json(brand_data);
+        } catch(error) {
+            console.error("Ha ocurrido un error", error);
+            res.status(500).json({message: "Ha ocurrido un error en el servidor"});
         }
     }
 
-    getByMarca = async (req: Request, res: Response<Model[] | { message: string } | {error:string}>): Promise<void> => {
-        const {id_marca} = req.params
+    createModel = async(req: Request, res: Response<Model | {message: string}>): Promise<void> => {
+        const id_marca = parseInt(req.params.id_marca, 10);
+        const {nombre, aceite_caja, aceite_motor, octanaje, tipo_refrigerante, peso_str, descripcion, nro_puesto_str} = req.body; 
+        
+        const peso = parseInt(peso_str, 10);
+        const nro_puesto = parseInt(nro_puesto_str,10);
 
         try{
-            const models = await ModelsModel.getByMarca(parseInt(id_marca))
+            const result: Model = await ModelsModel.createModel({id_marca, nombre, aceite_caja, aceite_motor, octanaje, tipo_refrigerante, peso, descripcion, nro_puesto}); 
 
-            if(!models){
-                res.status(404).json({error:'Modelos no encontrados'})
-                return
+            if(!result){
+                res.status(400).json({message: "No se pudo crear el modelo"});
+                return;
             }
-            res.status(200).send(models)
-        }catch (err){
-            console.log(err)
-            res.status(500).json({error:'Internal server error' + err})
-            return 
-        }
-    }
-    /*
-    create = async (req: Request, res: Response< { message: string } | {error:string}>): Promise<void> => {
-        const {id_marca,nombre,aceite_caja,aceite_motor,octanaje,tipo_refrigerante,descripcion,peso,nro_puestos} = req.body
 
-        try{
-            const resutl = await ModelsModel.createModel({id_marca,nombre,aceite_caja,aceite_motor,octanaje,tipo_refrigerante,peso,descripcion,nro_puestos})
+            res.status(201).json(result);
+        } catch(error){
+            console.error("Ha ocurrido un error", error);
+            res.status(500).json({message: ""});
         }
     }
-    */
+
+    
 }
