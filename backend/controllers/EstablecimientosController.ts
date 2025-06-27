9966db4581ff5a36066e8eeb6d260ed90bb2044b
FonsoP <ajpalma.23@est.ucab.edu.ve>
2025-06-27 17:01:41 -0400

    comi

--- backend/controllers/EstablecimientosController.ts
+++ backend/controllers/EstablecimientosController.ts
@@ -87,7 +87,9 @@ export class EstablishmentController{
             
             
         try {
-            const result = await establishmentsModel.add({ RIF, CI_PIC, name, city, date_PIC:new Date(date_PIC) });
+
+            const result = await establishmentsModel.add({ RIF, CI_PIC, name, city, date_PIC });
+            const result = await establishmentsModel.add({ RIF, CI_PIC, name, city, date_PIC: new Date(date_PIC) });
             res.status(201).json({ message: 'Establecimiento agregado con éxito.', ...result });
             return;
         } catch (error) {
