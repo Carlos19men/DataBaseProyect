1e99e27866d877fb19de4a8ea3bdfe08e614b4fa
ElJako11 <joaquimejuhah@gmail.com>
2025-07-05 00:37:43 -0400

    Brand, Services, Family, Activities Pages finished (Not tested)

---
+++ frontend/src/pages/RegistrarMarca/RegistrarMarca.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import TopBar from "../../components/TopBar/TopBar";
+import styles from "./RegistrarMarca.module.css";
+
+const RegistrarMarca: React.FC = () => {
+  const [nombre, setNombre] = useState("");
+
+  return (
+    <div className={styles.body}>
+      <TopBar text="Registrar Marca" menu={true} />
+      <div className={styles.content}>
+        <form className={styles.form}>
+          <div className={styles.inputGroupFull}>
+            <label className={styles.label}>Nombre:</label>
+            <input
+              className={styles.input}
+              type="text"
+              value={nombre}
+              onChange={e => setNombre(e.target.value)}
+              placeholder=""
+            />
+          </div>
+          <div className={styles.buttonContainer}>
+            <button type="submit" className={styles.button}>
+              Añadir
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrarMarca; 
